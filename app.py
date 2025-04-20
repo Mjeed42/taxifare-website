d456ff6bd8ab9ef228f6a915bbd88c4bc58c7e8b
Abdulmajeed Alrashidi <alrashabdulmajeed@gmail.com>
2025-04-20 13:54:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -159,6 +159,24 @@ if st.button("Get Fare Prediction", type="primary"):
  #       ).add_to(m)
  #       st_folium(m, width=1200, height=500)
 
+
+        map_output = st_folium(
+    create_map(),
+
+    width=1200,
+    height=500,
+
+)
+
+        folium.PolyLine(
+            locations=[st.session_state.pickup_coords, st.session_state.dropoff_coords],
+            color="green",
+            weight=5,
+            opacity=0.7
+        ).add_to(map_output)
+
+
+
     except :
         st.error(f"Error getting prediction: ")
         st.markdown('''
